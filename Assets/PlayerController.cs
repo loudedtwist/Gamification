52ef00229b588252ed2daf949d162649da89f27c
Vitali Drazdovich <loudedtwist@gmail.com>
2017-01-12 09:10:47 +0100

    LoginPage show user data

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+ 
+public class PlayerController : MonoBehaviour { 
+    void Update() {
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += new Vector3(-0.1f, 0.0f, 0.0f);
+
+        if (Input.GetKey(KeyCode.W))
+            gameObject.transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += new Vector3(0.0f, -0.1f, 0.0f);
+
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += new Vector3(0.1f, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/UserHomePage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Parse; 
+
+public class UserHomePage : MonoBehaviour {
+
+
+    private ParseUser user = null; 
+    public Text userName;
+    public Text sNummer;
+    public Text faculty;
+    public GuiManager guiManager;
+
+    void OnEnable(){
+        user = ParseUser.CurrentUser;
+        if (user == null)
+        {
+            guiManager.ShowLoginPage();
+            return;
+        } 
+        userName.text = user.Username;
+        sNummer.text = user.Get<string>("sNummer");
+        faculty.text = user.Get<string>("faculty");
+    }
+
+}

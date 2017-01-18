841063c0a767118f326be2a4e54533d3582d7334
Artjom Djaditschkin <belic93@yandex.ru>
2017-01-18 01:20:54 +0100

    network basics -> movement, shooting

--- Assets/NetworkTest/Scripts/MyPlayerController.cs
+++ Assets/NetworkTest/Scripts/MyPlayerController.cs
@@ -1,13 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class MyPlayerController : MonoBehaviour
+public class MyPlayerController : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+	    if (!isLocalPlayer) return;
+
+	    var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        CmdFire();
+	    }
 	}
+
+    [Command]
+    private void CmdFire()
+    {
+        var bullet = (GameObject) Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
 }

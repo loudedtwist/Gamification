3d66ac948a8d3a5ef49be2a108daee74431d0f87
Vitali Drazdovich <loudedtwist@gmail.com>
2017-01-18 14:18:04 +0100

    ~redesign

---
+++ Assets/PlayerName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Parse;
+
+public class PlayerName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.name = ParseUser.CurrentUser.Username;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/UserListInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserListInfo : MonoBehaviour {
+    public Text usersList;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update(){
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        if (players == null)
+        {
+            Debug.LogError("NO USERS TAG FOUND");
+            return;
+        }
+        string users = "";
+        foreach(var user in players){
+            users += user.gameObject.transform.name + "\n";
+        }
+        usersList.text  = users;
+    }
+}

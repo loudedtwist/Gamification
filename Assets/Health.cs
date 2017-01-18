fd8313f7d6d3010d463cd6bdaaf47aef7bffabe7
Artjom Djaditschkin <belic93@yandex.ru>
2017-01-18 02:40:18 +0100

    simple multiplayer for 2 players

---
+++ Assets/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Health : NetworkBehaviour
+{
+    public RectTransform healthBar;
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer) return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+            RpcRespawn();
+        }
+        OnChangeHealth(currentHealth);
+    }
+
+    private void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/NetworkTest/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hitedObject = collision.gameObject;
+        var health = hitedObject.GetComponent<Health>();
+        if(health != null) health.TakeDamage(10);
+        Destroy(gameObject);
+    }
+
+}

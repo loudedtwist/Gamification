79e0b706ac8b901a31ccb8fba5966d9cc41a3ed2
Vitali Drazdovich <loudedtwist@gmail.com>
2017-02-05 20:45:12 +0100

    +ranking

---
+++ Assets/Ranking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Parse;
+public class Ranking : MonoBehaviour {
+    public Text rankingText;
+
+    void OnEnable(){
+        rankingText.text="";
+        StartCoroutine(GetRanking());
+    }
+
+    IEnumerator GetRanking(){ 
+        var task = ParseUser.Query
+            .OrderByDescending("score")
+            .Limit(10)
+            .FindAsync();
+        while(!task.IsCompleted) yield return null;
+
+        IEnumerable<ParseUser> users = task.Result;
+        foreach(var user in users){
+            rankingText.text = rankingText.text + "\n" + user.Get<string>("username") + ": " + user.Get<int>("score");
+        }
+
+
+    }
+}
